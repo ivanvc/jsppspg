8026534f5f394e0fc9e7b829427157b69a4fb898
Gus Ortiz <guspidey@gmail.com>
2010-08-05 15:04:26 -0500

    Added profit formula

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ end
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/apuestas.sql")
 
 get '/' do
-  @teams = Team.all.map { |team| { :team => team, :bets => team.bets } } 
+  @teams = Team.all.map { |team| { :team => team, :bets => team.bets } }
   @total_bets = Bet.all.size
   @total_bets = 1 if @total_bets.zero?
   erb :index
@@ -22,15 +22,16 @@ end
 post '/' do
   bet = Bet.new(params[:bet])
   bet.save
-  redirect '/' 
+  redirect '/'
 end
 
-class Bet 
+class Bet
   include DataMapper::Resource
   property :id, Serial
   property :owner, String, :required => true
   property :amount, Integer, :required => true
   belongs_to :team
+
 end
 
 class Team
@@ -38,6 +39,12 @@ class Team
   property :id, Serial
   property :name, String, :required => true
   has n, :bets
+
+  def total_amount
+    total = 0
+    bets.each {|bet| total += bet.amount}
+    total
+  end
 end
 
 DataMapper.auto_upgrade!
