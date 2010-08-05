f3f6c34621671795ae864d28ce6beaad06f43c61
Iván Valdés (@ivanvc) <iv@nvald.es>
2010-08-05 12:26:03 -0500

    System

---
+++ app.rb
@@ -0,0 +1,45 @@
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-validations'
+require 'sinatra'
+require 'do_sqlite3'
+
+configure do
+  set :app_file, File.expand_path(File.dirname(__FILE__) + '/app.rb')
+  set :public,   File.expand_path(File.dirname(__FILE__) + '/public')
+  set :views,    File.expand_path(File.dirname(__FILE__) + '/views')
+  disable :run, :reload
+end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/apuestas.sql")
+
+get '/' do
+  @teams = Team.all.map { |team| { :team => team, :bets => team.bets } } 
+  @total_bets = Bet.all.size
+  @total_bets = 1 if @total_bets.zero?
+  erb :index
+end
+
+post '/' do
+  bet = Bet.new(params[:bet])
+  bet.save
+  redirect '/' 
+end
+
+class Bet 
+  include DataMapper::Resource
+  property :id, Serial
+  property :owner, String
+  property :amount, Integer
+  belongs_to :team
+end
+
+class Team
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  has n, :bets
+end
+
+DataMapper.auto_upgrade!
+
